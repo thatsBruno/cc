1c23c90f6d2a615d429549e497dc54591b8f209c
Bruno <brunoandreribeiro18@gmail.com>
2024-09-30 15:48:01 +0100

    step 2

--- wc/main.go
+++ wc/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	filePathPtr := flag.String("c", "", "file path")
+
+	flag.Parse()
+
+	file, err := os.Stat(*filePathPtr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileLines, err := os.Open(*filePathPtr) // replace with your file name
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer fileLines.Close()
+
+	scanner := bufio.NewScanner(fileLines)
+
+	lines := 0
+	for scanner.Scan() {
+		lines++
+	}
+
+	fmt.Println("File name: ", *filePathPtr)
+	fmt.Println("File size: ", file.Size())
+	fmt.Println("File lines: ", lines)
 }
