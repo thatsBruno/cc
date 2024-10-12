e13156bc1943cf715444cb32239217a25a1c9ce7
Bruno <brunoandreribeiro18@gmail.com>
2024-10-12 16:44:52 +0100

    added port scanner

---
+++ portScanner/go.mod
@@ -0,0 +1,3 @@
+module thatsbruno.portScanner
+
+go 1.22.1
---
+++ portScanner/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	port := flag.Int("port", 0, "file path")
+
+	flag.Parse()
+
+	scanPort(*port)
+}
+
+func scanPort(port int) {
+	addr := fmt.Sprintf("localhost:%d", port)
+
+	var d net.Dialer
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	conn, err := d.DialContext(ctx, "tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to dial: %v", err)
+	}
+	defer conn.Close()
+
+	if _, err := conn.Write([]byte("Hello, World!")); err != nil {
+		log.Fatal(err)
+	}
+}
